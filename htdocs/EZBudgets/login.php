a324e92abbc9e3013abb196c6aeb9dc1c83a7608
Lightningluster <82775417+Lightningluster@users.noreply.github.com>
2025-11-18 13:33:07 -0800

    Registration and Login working

---
+++ htdocs/EZBudgets/login.php
@@ -0,0 +1,35 @@
+<?php include("db_connect.php"); session_start(); ?>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="Username" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit" name="login">Login</button>
+</form>
+
+<?php
+if (isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = $conn->query("SELECT * FROM users WHERE username='$username'");
+
+    if ($result->num_rows > 0) {
+        $user = $result->fetch_assoc();
+
+        if (password_verify($password, $user["password"])) {
+            $_SESSION["username"] = $user["username"];
+            $_SESSION["first_name"] = $user["first_name"];
+            $_SESSION["last_name"] = $user["last_name"];
+
+            header("Location: dashboard.php");
+            exit;
+        } else {
+            echo "Incorrect password.";
+        }
+    } else {
+        echo "User not found.";
+    }
+}
+?>
+
+<p>Not a user yet? <a href="index.php">Register now!</a>.</p>
